da4377c72257c0e7269fde97e81b2f8d67f9e7f7
aseempurohit <purohitaseem@gmail.com>
2017-03-24 14:47:23 +0530

    Update service-time.py

--- service-time.py
+++ service-time.py
@@ -3,6 +3,9 @@ from flask_restful import Resource, Api
 import sys
 import time
 
+export http_proxy="http://165.225.104.34:80"
+export https_proxy="https://165.225.104.34:80"
+
 app = Flask(__name__)
 api = Api(app)
 
